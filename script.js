c83f62686fd6fdec2c8a232545a7389cb4250563
William <williamh29@nycstudents.net>
2023-09-28 20:19:32 -0400

    U2L1

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ function updateName() {
   const name = prompt("Enter a new name");
   button.textContent = `Player 1: ${name}`;
 }
+const button2 = document.getElementById("button2")
+
+
+button2.addEventListener("click", updateHeight);
+
+
+function updateHeight(){
+  const height = prompt("Enter new height");
+  button2.textContent = `Player 1: ${height}`;
+}
+
+
